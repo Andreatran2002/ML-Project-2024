7fe5341f2e19cd0391f0afc6f224719fb3c21f9e
Phuong Vy <andreatran2002@gmail.com>
2024-11-21 14:05:05 +0700

    add chicken image

---
+++ checkmodel.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 22 15:31:57 2022
+
+@author: tuann
+"""
+from torchsummary import summary
+from models.Group6Net import *
+model = Net()
+model = model.cuda()
+print ("model")
+print (model)
+
+# get the number of model parameters
+print('Number of model parameters: {}'.format(
+    sum([p.data.nelement() for p in model.parameters()])))
+#print(model)
+#model.cuda()
+summary(model, (3, 224, 224))
+#summary(model, (3, 32, 32))
